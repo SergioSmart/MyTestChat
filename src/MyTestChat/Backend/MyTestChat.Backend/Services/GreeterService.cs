4f881000b9f2b653b53bfcb69859df09c3fe678c
SergioSmart <serejja2018@gmail.com>
2022-08-26 17:58:22 +0300

    Settings for greet.proto

--- src/MyTestChat/Backend/MyTestChat.Backend/Services/GreeterService.cs
+++ src/MyTestChat/Backend/MyTestChat.Backend/Services/GreeterService.cs
@@ -1,5 +1,6 @@
 using Grpc.Core;
 using MyTestChat.Backend;
+using MyTestChat.Protos;
 
 namespace MyTestChat.Backend.Services
 {
@@ -8,14 +9,14 @@ namespace MyTestChat.Backend.Services
         private readonly ILogger<GreeterService> _logger;
         public GreeterService(ILogger<GreeterService> logger)
         {
-            _logger = logger;
+            _logger = logger;    
         }
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
             {
-                Message = "Hello " + request.Name
+                Message = "SMS from " + request.Name
             });
         }
     }
