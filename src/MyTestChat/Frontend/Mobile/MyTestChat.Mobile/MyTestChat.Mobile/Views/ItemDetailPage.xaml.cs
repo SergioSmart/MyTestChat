d69cd2c44a32c6907c37639db06637125cdc755d
SergioSmart <serejja2018@gmail.com>
2022-08-26 18:38:47 +0300

    Added Blazor WebAssembly

---
+++ src/MyTestChat/Frontend/Mobile/MyTestChat.Mobile/MyTestChat.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyTestChat.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MyTestChat.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
