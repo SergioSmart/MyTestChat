0a80234cb02a6eacddc1bf27a5baa94f1ac70a3f
SergioSmart <serejja2018@gmail.com>
2022-08-27 02:15:51 +0300

    Settings for gRPC's Program.cs

--- src/MyTestChat/Backend/MyTestChat.Backend/Program.cs
+++ src/MyTestChat/Backend/MyTestChat.Backend/Program.cs
@@ -16,18 +16,18 @@ namespace MyTestChat.Backend
             
             var app = builder.Build();
             
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-            //app.UseBlazorFrameworkFiles();
+            app.UseBlazorFrameworkFiles();
 
-            //app.UseStaticFiles();
+            app.UseStaticFiles();
 
-            //app.UseRouting();
+            app.UseRouting();
 
             // Configure the HTTP request pipeline.
             app.MapGrpcService<GreeterService>();
-            app.MapGet("/", () => "GreEtings!!! Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+            //app.MapGet("/", () => "GreEtings!!! Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            app.MapFallbackToFile("index.html");
             app.Run();
         }
     }
